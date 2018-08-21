72d7d9e1810bd90c6e2e010222f151267e430bb1
Kenneth Solano <klsolano77@gmail.com>
2018-08-20 18:54:11 -0800

    Oh boy

--- practiceCodingBeginning.js
+++ practiceCodingBeginning.js
@@ -9,3 +9,19 @@ for (var i = 10; i>0;i--){
 for (var i =1; i<11;i++){
     console.log(i);
 }
+
+// What country is your favorite?
+
+console.log("Peru")
+
+//Practice 
+
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
+
+console.log(21+3.5);
+console.log(2018-1969);
+console.log(65/240);
+console.log(100*0.2708);
