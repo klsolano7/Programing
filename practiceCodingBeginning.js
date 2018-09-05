185fa6a152998f76d9b753aae561ac1daf497ae1
Kenneth Solano <klsolano77@gmail.com>
2018-09-05 15:38:13 -0800

    YDKJS

--- practiceCodingBeginning.js
+++ practiceCodingBeginning.js
@@ -65,10 +65,8 @@ console.log('Goal!');
 
 //08/30/2018 continuing to watch video and practice on acedemy
 
-let sale = true;
+Let sale = false
 
-sale = false;
-
-if (sale) {
-  console.log('Time to buy!');
+let (sale === false) {
+  console.log('Let go do some shopping!')
 }
